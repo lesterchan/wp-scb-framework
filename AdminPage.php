972963d39d05242c80f23a6567f46e5e471d7ccc
Lester Chan <lesterchan@gmail.com>
2018-02-24 14:48:41 +0800

    Remove screen_icon()

--- AdminPage.php
+++ AdminPage.php
@@ -186,7 +186,6 @@ abstract class scbAdminPage {
 	 */
 	protected function page_header() {
 		echo "<div class='wrap'>\n";
-		screen_icon( $this->args['screen_icon'] );
 		echo html( 'h2', $this->args['page_title'] );
 	}
 
