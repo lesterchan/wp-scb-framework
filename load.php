dbdf8142fabe80c494703cf88f6e80cdea0f9cf2
Peter Muriuki <git-pete@phlixeprojects.co.uk>
2024-11-16 16:12:13 +0000

    Fix to prevent errors with just in time translation loading occurring too early.

--- load.php
+++ load.php
@@ -39,10 +39,10 @@ class scbLoad4 {
 			add_action( 'activate_plugin',  array( __CLASS__, 'delayed_activation' ) );
 		}
 
-		if ( did_action( 'plugins_loaded' ) ) {
+		if ( did_action( 'init' ) ) {
 			self::load();
 		} else {
-			add_action( 'plugins_loaded', array( __CLASS__, 'load' ), 9, 0 );
+			add_action( 'init', array( __CLASS__, 'load' ), 9, 0 );
 		}
 	}
 
